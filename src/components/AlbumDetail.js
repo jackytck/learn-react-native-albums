e72eb27c1c1a78e4807b4af91ca3793d96d78beb
Jacky Tang <jackytck@gmail.com>
2017-03-04 11:40:51 +0800

    :sparkles: add AlbumDetail component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react'
+import {
+  View,
+  Text
+} from 'react-native'
+
+const AlbumDetail = (props) => {
+  return (
+    <View>
+      <Text>{props.album.title}</Text>
+    </View>
+  )
+}
+
+AlbumDetail.propTypes = {
+  album: PropTypes.object.isRequired
+}
+
+export default AlbumDetail
